60bafa38d74dde44c6cb649e3d4f0077060155af
guoziqi <919817489@qq.com>
2021-02-19 22:14:54 +0800

    答题： Kotlin 基础

--- Kotlin/app/src/main/java/com/example/app/widget/CodeView.kt
+++ Kotlin/app/src/main/java/com/example/app/widget/CodeView.kt
@@ -1,45 +1,20 @@
-package com.example.app.widget;
+package com.example.app.widget
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.Gravity;
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatTextView
+import com.example.app.R
+import com.example.core.utils.Utils
+import java.util.*
 
-import com.example.app.R;
-import com.example.core.utils.Utils;
-
-import java.util.Random;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatTextView;
-
-public class CodeView extends AppCompatTextView {
-
-    public CodeView(Context context) {
-        this(context, null);
-    }
-
-    public CodeView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f);
-        setGravity(Gravity.CENTER);
-        setBackgroundColor(getContext().getColor(R.color.colorPrimary));
-        setTextColor(Color.WHITE);
-
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(getContext().getColor(R.color.colorAccent));
-        paint.setStrokeWidth(Utils.dp2px(6f));
-
-        updateCode();
-    }
-
-    private Paint paint = new Paint();
-
-    private String[] codeList = new String[]{
+class CodeView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null) : AppCompatTextView(context, attrs) {
+    private val paint = Paint()
+    private val codeList = arrayOf(
             "kotlin",
             "android",
             "java",
@@ -48,19 +23,28 @@ public class CodeView extends AppCompatTextView {
             "okhttp",
             "retrofit",
             "tcp/ip"
-    };
+    )
 
-
-    public void updateCode() {
-        final int random = new Random().nextInt(codeList.length);
-        final String code = codeList[random];
-        setText(code);
+    fun updateCode() {
+        val random = Random().nextInt(codeList.size)
+        val code = codeList[random]
+        text = code
     }
 
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawLine(0f, height.toFloat(), width.toFloat(), 0f, paint)
+        super.onDraw(canvas)
+    }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawLine(0f, getHeight(), getWidth(), 0f, paint);
-        super.onDraw(canvas);
+    init {
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+        gravity = Gravity.CENTER
+        setBackgroundColor(getContext().getColor(R.color.colorPrimary))
+        setTextColor(Color.WHITE)
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.STROKE
+        paint.color = getContext().getColor(R.color.colorAccent)
+        paint.strokeWidth = Utils.dp2px(6f)
+        updateCode()
     }
 }
--- Kotlin/base/src/main/java/com/example/core/BaseApplication.kt
+++ Kotlin/base/src/main/java/com/example/core/BaseApplication.kt
@@ -1,22 +1,18 @@
-package com.example.core;
+package com.example.core
 
-import android.app.Application;
-import android.content.Context;
+import android.app.Application
+import android.content.Context
 
-import androidx.annotation.NonNull;
-
-public class BaseApplication extends Application {
-
-    private static Context currentApplication;
-
-    @NonNull
-    public static Context currentApplication() {
-        return currentApplication;
+class BaseApplication : Application() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        currentApplication = this
     }
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        currentApplication = this;
+    companion object {
+        private var currentApplication: Context? = null
+        fun currentApplication(): Context {
+            return currentApplication!!
+        }
     }
 }
